03ee1cd87edc2eead2511e5048f47a2bbe90c661
Justin Slack <justin@newmedialabs.co.za>
2019-12-13 09:53:20 +0200

    Add gulp pipeline for sass

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+
+const gulp      = require("gulp");
+const sass      = require("gulp-sass");
+const uglify    = require('gulp-uglify');
+const concat    = require('gulp-concat');
+const watch     = require('gulp-watch')
+
+
+// a task to generate the css with sass
+gulp.task('css', function () {
+  return gulp.src('./_includes/assets/css/sass/main.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    })
+      .on('error', sass.logError))
+    .pipe(gulp.dest('./_includes/assets/css'));
+});
+/*
+  Watch folders for changes
+*/
+gulp.task("watch", function() {
+  gulp.watch("./_includes/assets/css/sass/main.scss", gulp.parallel("css"));
+});
+
+
+/*
+  Let's build this sucker.
+*/
+gulp.task('build', gulp.parallel(
+  'css'
+));
