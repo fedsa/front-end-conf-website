16e50aafe7a6d38c72f87fd18b0fc1bdfe10a2fe
Justin Slack <justin@newmedialabs.co.za>
2020-01-02 15:24:21 +0200

    Add font path

--- .eleventy.js
+++ .eleventy.js
@@ -74,7 +74,7 @@ module.exports = function(eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/images");
+  eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
   eleventyConfig.addPassthroughCopy("site.webmanifest");
